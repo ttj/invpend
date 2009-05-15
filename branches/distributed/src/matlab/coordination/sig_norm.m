84272f6979a334243c094106c87f4f1bc75167cf
taylor.johnson <taylor.johnson@6b3aee90-c63a-11dd-8772-d1b041bf0e37>
2009-05-15 21:10:59 +0000

    minor changes for ge525 project to flocking simulation; added final report for ge525 project and all plots

--- branches/distributed/src/matlab/coordination/sig_norm.m
+++ branches/distributed/src/matlab/coordination/sig_norm.m
@@ -1,4 +1,4 @@
 function [out] = sig_norm(z, epsilon)
-    %out = norm(z,2);
-    out = (1 / epsilon) * (sqrt(1 + epsilon * (norm(z,2))^2)-1);
+    out = norm(z,2);
+    %out = (1 / epsilon) * (sqrt(1 + epsilon * (norm(z,2))^2)-1);
 end
