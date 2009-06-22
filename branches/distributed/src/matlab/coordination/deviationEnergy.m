ff82263cb01b12ebf23f1a71558229047cdfc773
taylor.johnson <taylor.johnson@6b3aee90-c63a-11dd-8772-d1b041bf0e37>
2009-06-22 21:47:09 +0000

    primarily added error/energy functions and plots for stability analysis reasoning; added alpha function plots

--- branches/distributed/src/matlab/coordination/deviationEnergy.m
+++ branches/distributed/src/matlab/coordination/deviationEnergy.m
@@ -1,13 +1,15 @@
-function E = deviationEnergy(q, r, d, delta)
-    %n = size(q,1); %# agents
-    %m = size(q,2); %# dimensions
-    E = zeros(size(q));
-    for i=1:size(q,1)
-        %js=neighborsSpatialLattice(i, q(i,:), q, r, d, delta);
-        js=neighborsSpatial(i, q(i,:), q, r, d);
-        for j=1:size(js)
-            E(i,:) = E(i,:) + psi(norm(q(j,:) - q(i,:), 2) - d);
-        end
-    end
+function E = deviationEnergy(q, q_goal, r_comm, r_lattice, delta)
+    n = size(q,1); %# agents
+    m = size(q,2); %# dimensions
+%     E = zeros(size(q));
+%     for i=1:size(q,1)
+%         %js=neighborsSpatialLattice(i, q(i,:), q, r, d, delta);
+%         js=neighborsSpatial(i, q(i,:), q, r, d);
+%         for j=1:size(js)
+%             E(i,:) = E(i,:) + psi(norm(q(j,:) - q(i,:), 2) - d);
+%         end
+%     end
     %E = E .* (1/(norm(proximityNetEdges(q, r, d),1) + 1));
+
+    E = abs(q(:,:) - q_goal(:,:));
 end
