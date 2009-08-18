5501f09186628bc528397871cbc6046d86769cf3
taylor.johnson <taylor.johnson@6b3aee90-c63a-11dd-8772-d1b041bf0e37>
2009-08-18 20:14:41 +0000

    August 18, 2009: Added potential fix for difference equation of node N, as previously it had violated safety in some cases, such as x_1[0] = 0, x_2[0] = 10, x_3[0] = 1000, for d = 100, then x_1[1] = 0, x_2[1] = (0 + 1000)/2, x_3[1] = 10 + 100.  The new difference equation for x_N[k+1]=(x_{N-1}[k] + x_N[k] + d)/2, which solves the above safety problem when assuming r_s <= d/2.

--- src/matlab/coordination/checkSpacingInvariant.m
+++ src/matlab/coordination/checkSpacingInvariant.m
@@ -2,8 +2,12 @@ function [ out ] = checkSpacingInvariant(q, spacing)
     N = size(q, 1);
     count = 0;
     for i = 2 : N
-        if (norm(q(i) - q(i - 1), 2) < spacing) || q(i - 1) >= q(i)
+        if (norm(q(i) - q(i - 1), 2) < spacing)
             count = count + 1;
+            strcat('spacing: nodes ', int2str(i), ' and ', int2str(i-1), ' with spacing ', num2str(norm(q(i) - q(i - 1), 2)), '<', num2str(spacing))
+        elseif q(i - 1) >= q(i)
+            count = count + 1;
+            strcat('collision: nodes ', int2str(i), ' and ', int2str(i-1), ' with spacing ', num2str(norm(q(i) - q(i - 1), 2)), '<', num2str(spacing))
         end
     end
     
